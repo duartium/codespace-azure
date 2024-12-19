1c1885d03f6cf06ad8d4f1ed5bae2366008514bb
Byron Duarte <byronduarte95@gmail.com>
2024-12-19 19:47:29 +0000

    authentication microsoft identity

---
+++ src/a204-auth/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+private const string _clientId = "APPLICATION_CLIENT_ID";
+private const string _tenantId = "DIRECTORY_TENANT_ID";
+
+var app = PublicClientApplicationBuilder
+    .Create(_clientId)
+    .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+    .WithRedirectUri("http://localhost")
+    .Build();
+
+string[] scopes = { "user.read" };
+
+//request token
+AuthenticationResult result = await app.AcquireTokenInteractive(scopes)
+    .ExecuteAsync();
+
+Console.WriteLine($"Token:\t{result.AccesToken}");
